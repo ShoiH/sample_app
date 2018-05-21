b55558aa2963e7404092be6569839289cd7e2207
ShoiH <wdlove1008sekai@gmail.com>
2018-05-21 17:51:29 +0900

    renamed:    layout_file -> app/views/layouts/application.html.erb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,6 +12,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title" , "Home | #{@base_title}"
   end
 
+  test "should get Contact" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title" , "Home | #{@base_title}"
+  end
+ 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
